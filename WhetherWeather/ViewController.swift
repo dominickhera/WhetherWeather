12112e74b2d787e918b1919264cf4e52e167c9e2
dominickhera <dominickhera@me.com>
2018-07-03 12:46:59 -0400

    Initial Commit

---
+++ WhetherWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhetherWeather
+//
+//  Created by Dominick Hera on 7/3/18.
+//  Copyright © 2018 Dominick Hera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
