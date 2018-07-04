a1ea5656e85b9edd85c7f0da22bd5b1a716eae13
dominickhera <dominickhera@me.com>
2018-07-04 15:44:38 -0400

    got temp working

--- WhetherWeather/ViewController.swift
+++ WhetherWeather/ViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import CoreLocation
+import OpenWeatherMapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager: CLLocationManager!
+    
+    @IBOutlet weak var weatherConditionLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+         OpenWeatherMapKit.initialize(withAppId: "b8ae01f6344f6fb00b128c10e4ffcf19")
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func requestLocation(_ sender: Any) {
+        var currentLocation: CLLocation!
+        var currentTemp: Double!
+        var weatherCondition: String!
+        if( CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() ==  .authorizedAlways){
+
+//            latitudeString = locationManager.location?.coordinate.latitude
+
+            currentLocation = locationManager.location
+
+                OpenWeatherMapKit.instance.currentWeather(forCoordiante: (latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)) { (forecast, error) in
+
+                currentTemp = forecast?.main.celsius.currentTemp
+                    weatherCondition = forecast?.weather.description
+
+                    DispatchQueue.main.async {
+                                    self.currentTempLabel.text = "\(round(currentTemp!))"
+//                            self.weatherConditionLabel.text = weatherCondition.description
+                        print(weatherCondition!)
+                        for item in weatherCondition! {
+                            print("\(item.description)")
+                        }
+
+                    }
+//                print(forecast?.main.celsius.currentTemp)
+            }
+
+        }
+
     }
 
 
